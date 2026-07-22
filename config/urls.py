0e4c13da66205b1a0b4a3229c6a0bfa749bbb8c0
shivaydwivedi <polymath1510@gmail.com>
2026-07-22 16:51:48 +0530

    feat: enable django admin

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,13 @@
 
 from __future__ import annotations
 
+from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("", include("apps.core.urls")),
+    path("admin/", admin.site.urls),
     path("api/v1/auth/", include("apps.accounts.urls")),
     path("api/v1/", include("apps.bookmarks.urls")),
     path("api/v1/", include("apps.comments.urls")),
